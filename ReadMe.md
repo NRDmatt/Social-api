d54d7d9c40868c2efd0dc158e33d10b4334263c6
matt <mathewandnana@gmail.com>
2023-04-18 12:03:57 -0500

    first commit

---
+++ ReadMe.md
@@ -0,0 +1,41 @@
+# Social network API
+
+![MIT License](https://img.shields.io/badge/License-MIT-yellow.svg)
+
+
+## Description
+
+
+With this application you can add delete update and test routes in insomia 
+
+
+## Table of Contents
+
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Questions](#questions)
+
+
+## Installation
+
+
+To install necessary dependencies, run the following command:
+
+
+```
+"npm i" and "npm start" put routes in insomia
+```
+
+
+## Usage
+
+
+the user can submit large amounts of unstructured data
+
+
+
+## License
+
+![MIT License](https://img.shields.io/badge/License-MIT-yellow.svg) [MIT](https://opensource.org/licenses/MIT)
---
+++ models/User.js
@@ -0,0 +1,36 @@
+const {Schema, model} = require("mongoose");
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, "Your email was Wrong,please enter a valid email address",]
+    },
+    thoughts: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Thought"
+        },
+    ],
+    friends: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "User"
+        },
+    ]
+}, {
+    toJSON: {
+        virtuals: true,
+        getters: true
+    },
+    id: false
+});
+
+module.exports = User;
